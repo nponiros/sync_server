ade7532b81f536e696ac5b6dd5621d192725352d
Nikolas Poniros <nikolas@poniros.de>
2015-12-15 16:49:34 +0100

    refactor to use a base url

--- test/integration/check_spec.js
+++ test/integration/check_spec.js
@@ -4,19 +4,21 @@ const hippie = require('hippie');
 
 const settings = require('../test_settings');
 const url = `${settings.test.protocol}${settings.test.domain}:${settings.port}`;
-const apiUrl = `${url}${settings.apiPath}/check`;
+const apiPath = '/api/v1/check';
 
-describe('/check', () => {
+describe(apiPath, () => {
   it('should respond with a status of 200', (done) => {
     hippie()
-      .head(apiUrl)
+      .base(url)
+      .head(apiPath)
       .expectStatus(200)
       .end(done);
   });
 
   it('should respond with an empty body', (done) => {
     hippie()
-      .head(apiUrl)
+      .base(url)
+      .head(apiPath)
       .expectBody('')
       .end(done);
   });
