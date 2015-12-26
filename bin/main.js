49fc7bdd4af990230083a449c807723a06f4e4d1
Nikolas Poniros <nikolas@poniros.de>
2015-12-26 18:13:51 +0100

    Allow cli arguments for port, data path and collections

--- bin/main.js
+++ bin/main.js
@@ -2,7 +2,23 @@
 
 'use strict';
 
-const settings = require('../lib/settings.js');
+const argv = require('yargs').argv;
+
+const defaultSettings = require('../lib/settings.js');
 const server = require('../lib/server.js');
+
+let collectionNames;
+if (argv.collections) {
+  collectionNames = argv.collections.split(',');
+} else {
+  collectionNames = defaultSettings.collectionNames;
+}
+
+const settings = {
+  apiPath: defaultSettings.apiPath,
+  port: argv.p || defaultSettings.port,
+  dataPath: argv.path || defaultSettings.dataPath,
+  collectionNames
+};
 
 server.start(settings);
