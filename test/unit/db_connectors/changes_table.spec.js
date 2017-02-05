74120994ce6eca34b7a38d610dcf55e641d8b826
Nikolas Poniros <nikolas@poniros.de>
2017-02-05 16:26:53 +0100

    Fix linting

--- test/unit/db_connectors/changes_table.spec.js
+++ test/unit/db_connectors/changes_table.spec.js
@@ -7,7 +7,7 @@ const ChangesTable = require('../../../lib/db_connectors/NeDB/changes_table');
 
 describe('changesTable', () => {
   it('should be able to cope with mods including dots when adding a change', (done) => {
-    const changesTable = new ChangesTable('changes', {inMemoryOnly: true});
+    const changesTable = new ChangesTable('changes', { inMemoryOnly: true });
     changesTable
       .add({ mods: { 'foo.bar': 'foo', 'bar': 'baz', 'feb.bar': 'fe' } })
       .then(() => {
@@ -29,7 +29,7 @@ describe('changesTable', () => {
   });
 
   it('should replace the dotReplacement with a real dot when changes are read getByRevision()', (done) => {
-    const changesTable = new ChangesTable('changes', {inMemoryOnly: true});
+    const changesTable = new ChangesTable('changes', { inMemoryOnly: true });
     const changeObject = {
       mods: {
         [`foo${changesTable.dotReplacer}bar`]: 'foo',
@@ -40,7 +40,7 @@ describe('changesTable', () => {
     };
     changesTable.store.insert(changeObject, (err) => {
       if (err) {
-        return done(err)
+        return done(err);
       }
       changesTable.getByRevision(0)
         .then((data) => {
@@ -58,18 +58,18 @@ describe('changesTable', () => {
   });
 
   it('should replace the dotReplacement with a real dot when changes are read getByRevisionAndClientID()', (done) => {
-    const changesTable = new ChangesTable('changes', {inMemoryOnly: true});
+    const changesTable = new ChangesTable('changes', { inMemoryOnly: true });
     const changeObject = {
       mods: {
         [`foo${changesTable.dotReplacer}bar`]: 'foo',
         bar: 'baz',
-        [`feb${changesTable.dotReplacer}bar`]: 'fe',      },
+        [`feb${changesTable.dotReplacer}bar`]: 'fe' },
       rev: 1,
       source: 0,
     };
     changesTable.store.insert(changeObject, (err) => {
       if (err) {
-        return done(err)
+        return done(err);
       }
       changesTable.getByRevisionAndClientID(0, 1)
           .then((data) => {
